b435fe5be8254e4b7a17a761a9c2988241faee8d
Bogdan Poplauschi <bpoplauschi@gmail.com>
2014-05-12 17:24:07 +0300

    Removed unused code

--- Example/CrashlyticsFrameworkTest/CrashlyticsFrameworkTest/BPViewController.m
+++ Example/CrashlyticsFrameworkTest/CrashlyticsFrameworkTest/BPViewController.m
@@ -29,16 +29,4 @@
 
 @implementation BPViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
